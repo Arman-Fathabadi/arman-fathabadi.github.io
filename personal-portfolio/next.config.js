d55cf2e38c6855539185af3fd91cad6aa72c7850
Arman Fathabadi <111296313+Arman-Fathabadi@users.noreply.github.com>
2025-02-11 18:34:10 -0500

    Update repository configuration

--- personal-portfolio/next.config.js
+++ personal-portfolio/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/personal-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-portfolio/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/personal_portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal_portfolio/' : '',
   images: {
     unoptimized: true
   },
